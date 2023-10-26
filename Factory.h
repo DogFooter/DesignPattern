fe07f446d7c5bfb658cc8c16d71a89eed54239e3
DogFooter <tmflffj4663@gmail.com>
2023-10-26 22:21:26 +0900

    FACTORY

---
+++ Factory.h
@@ -0,0 +1,23 @@
+#pragma once
+class IStruct {
+
+};
+
+template <typename T, typename P>
+class Factory {
+protected:
+	virtual T createProduct(P input) = 0;
+	virtual void registerProduct(T product) = 0;
+
+public:
+	T create(P input) final {
+		T p = createProduct(input);
+		registerProduct(p);
+		return p;
+	}
+};
+
+
+
+
+};
